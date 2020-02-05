eb1b21259e618d45db3322f9215f1eb27622a6d0
WISUNGWON <tkrhkaos12@naver.com>
2020-02-05 19:45:42 +0900

    Add Java Flies

--- Input/src/Main.java
+++ Input/src/Main.java
@@ -4,26 +4,40 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+
+
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = Integer.parseInt(br.readLine().trim());
-		int z = n;
 		
-		for (int i=1; i<=n+1; i++) {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			String n = br.readLine();
+			String[] test = n.split(" ");
+			int a = Integer.parseInt(test[0]); 
+			int b = Integer.parseInt(test[1]); 
+			String number = br.readLine();
+			String[] StringArr = number.split(" ");
+			int[] numberArr = new int[a];
 			
-			bw.write(z+ "\n");
-			z = z-1;
 			
-		}
+			for (int i = 0; i<a; i++) {
+				numberArr[i] = Integer.parseInt(StringArr[i]);
 		
-		bw.flush();
-		bw.close();
+			}
+			
 	
-		
+			for (int j=0; j<a; j++) {
+				if (numberArr[j] < b) {
+					bw.write(numberArr[j] + " ");
+				}
+				
+			}
+			
+			
+			bw.flush();
+			bw.close();
 		
 	}
 }

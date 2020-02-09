407c2ea8156c9534fb869491aa3c0b17c7e13f51
WISUNGWON <tkrhkaos12@naver.com>
2020-02-09 21:49:38 +0900

    Add java Files

--- Input/src/Main.java
+++ Input/src/Main.java
@@ -1,43 +1,20 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
-
-
+import java.util.Scanner;
+ 
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-		
-		
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-			String n = br.readLine();
-			String[] test = n.split(" ");
-			int a = Integer.parseInt(test[0]); 
-			int b = Integer.parseInt(test[1]); 
-			String number = br.readLine();
-			String[] StringArr = number.split(" ");
-			int[] numberArr = new int[a];
-			
-			
-			for (int i = 0; i<a; i++) {
-				numberArr[i] = Integer.parseInt(StringArr[i]);
-		
-			}
-			
-	
-			for (int j=0; j<a; j++) {
-				if (numberArr[j] < b) {
-					bw.write(numberArr[j] + " ");
-				}
-				
-			}
-			
-			
-			bw.flush();
-			bw.close();
-		
-	}
+ 
+    public static void main(String[] args) throws IOException{
+        // TODO Auto-generated method stub
+    	
+    	Scanner s = new Scanner(System.in);
+    	
+    	while(s.hasNextInt()) {
+    		int a = s.nextInt();
+    		int b = s.nextInt();
+    		
+    		System.out.println(a+b);
+    	}
+    }
 }
+

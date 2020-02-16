b26d2c47f8c132cb308a903abf16872153f280bf
WISUNGWON <tkrhkaos12@naver.com>
2020-02-16 11:51:48 +0900

    Add Java Files

--- Input/src/Main.java
+++ Input/src/Main.java
@@ -1,41 +1,48 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
- 
 public class Main {
- 
-    public static void main(String[] args) throws IOException{
-    	
-    
-    	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    	        int n = Integer.parseInt(br.readLine().trim());
-    	        int[] arr = new int[n];
-    	        StringTokenizer st = new StringTokenizer(br.readLine());
-    	        for (int i=0; i < n; i++) {
-    	            arr[i] = Integer.parseInt(st.nextToken());
-    	            bw.write((arr[i]) + " ");
-    	        }
-    	        
-    	        bw.flush();
-    	        bw.close();
-    	    
-     }	
-    		
-   
-}   	
-    		
- /*
-  *     arr[i] = Integer.parseInt(br.readLine().trim());
-    	            String[] word = text.split(" ");
-    	            int a = Integer.parseInt(word[0]);
-    	            int b = Integer.parseInt(word[1]);
-    	            bw.write((a+b) + "\n");
-  */
-    		
+public static void main(String[] args) {
+	System.out.println(solution("PoooPyy"));
+}
 
+private static boolean solution (String s) {
+    boolean answer = true;
+    s = s.toUpperCase(); //문자열을 대문자로 세팅 
+    String[] arr; 
+    arr = s.split("");//문자열의 요소를 하나하나 넣어줄 배열 만들고
+    int cntP = 0;
+    int cntY = 0; //y와 p의 대수를 count할 변수 만들어주고 
+    
+    System.out.println(Arrays.toString(arr));
+    System.out.println(arr[0] == Character.toString('P')); //왜 false가 나오는지 모르겠다.
+    System.out.println(arr[0]);
+     System.out.println(Character.toString('P'));
+    
+    for (int i = 0; i<s.length(); i++){ //요소를 하나하나 비교하여 p값과 y값 카운팅
+        if (arr[i] == Character.toString('P')){
+            cntP = cntP + 1;
+        }
+        else if (arr[i] ==Character.toString('Y')){
+            cntY = cntY + 1;
+        }
+    }
+     
+    System.out.println(cntP);
+    System.out.println(cntY);
+    if (cntP == cntY){
+        answer = true;
+    } else answer = false;
+    
+
+
+    return answer;
+
+ }
+
+}
+
+class Solution {
+	
+}

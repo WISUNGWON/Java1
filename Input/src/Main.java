3d8be71d5f589b9f20704e6bbd41a55764d5087e
WISUNGWON <tkrhkaos12@naver.com>
2020-02-21 12:34:02 +0900

    Add Java File

--- Input/src/Main.java
+++ Input/src/Main.java
@@ -4,45 +4,32 @@ import java.util.Scanner;
 
 public class Main {
 public static void main(String[] args) {
-	System.out.println(solution("PoooPyy"));
-}
+	String[] participant = 	{"leo", "kiki", "eden"};
+	String[] completion = {"eden", "kiki"};
+	Solution s1 = new Solution();
+	System.out.println(s1.solution(participant, completion));
 
-private static boolean solution (String s) {
-    boolean answer = true;
-    s = s.toUpperCase(); //문자열을 대문자로 세팅 
-    String[] arr; 
-    arr = s.split("");//문자열의 요소를 하나하나 넣어줄 배열 만들고
-    int cntP = 0;
-    int cntY = 0; //y와 p의 대수를 count할 변수 만들어주고 
-    
-    System.out.println(Arrays.toString(arr));
-    System.out.println(arr[0] == Character.toString('P')); //왜 false가 나오는지 모르겠다.
-    System.out.println(arr[0]);
-     System.out.println(Character.toString('P'));
-    
-    for (int i = 0; i<s.length(); i++){ //요소를 하나하나 비교하여 p값과 y값 카운팅
-        if (arr[i] == Character.toString('P')){
-            cntP = cntP + 1;
-        }
-        else if (arr[i] ==Character.toString('Y')){
-            cntY = cntY + 1;
-        }
-    }
-     
-    System.out.println(cntP);
-    System.out.println(cntY);
-    if (cntP == cntY){
-        answer = true;
-    } else answer = false;
-    
-
-
-    return answer;
-
+	
  }
-
 }
 
 class Solution {
-	
+    public String solution(String[] participant, String[] completion) {
+        String answer = ""; //완주하지 못한 선수 
+        for (int i = 0; i < participant.length; i++){
+            for (int j = 0; j < completion.length; j++){
+                if (participant[i].equals(completion[j])){
+                	participant[i] ="";
+                } else answer = participant[i];
+            }
+        }
+        return answer;
+    }
 }
+
+
+
+
+
+
+

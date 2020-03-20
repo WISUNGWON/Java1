331a02dcff9655b3a3f4277e655f449b3ab5f00d
WISUNGWON <tkrhkaos12@naver.com>
2020-03-20 21:39:30 +0900

    Add Java Files

---
+++ javaoop/Chapter7/src/hashmap/MemberHashMap.java
@@ -0,0 +1,13 @@
+package hashmap;
+
+import java.util.HashMap;
+
+public class MemberHashMap {
+    
+    private HashMap<Integer, Member> hashMap;
+    
+    public MemberHashMap() {
+        hashMap = new HashMap<Integer, Member>();
+    }
+
+}
